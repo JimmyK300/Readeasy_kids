8d0e41b8fa91bc4cf1d203e6ed52c099ede34997
JimmyK300 <134896739+JimmyK300@users.noreply.github.com>
2024-10-18 18:51:19 +0700

    almost finished home page

---
+++ src/LessonsComponents/EachLesson.tsx
@@ -0,0 +1,26 @@
+import "../App.css";
+import "bootstrap/dist/css/bootstrap.css";
+
+interface Props {
+  lessonNumber: string;
+  onClick: () => void;
+}
+
+const LessonButton = ({ lessonNumber = "Lesson 0", onClick }: Props) => {
+  return (
+    <div className="OuterLessonCover">
+      {/* <Image></Image> */}
+      <div style={{ font: "30px bold", flexGrow: "1" }}>{lessonNumber}</div>
+      <div className="ButtonDiv">
+        <button
+          className="btn btn-outline-success btn-lg StartButton"
+          onClick={onClick}
+        >
+          Start
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default LessonButton;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import LessonList from '../LessonsComponents/LessonList';
+
+function Home() {
+  return (
+    <LessonList />
+  );
+}
+
+export default Home;
